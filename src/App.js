1c416376f2295f3187553f07cbc83e700c9039ab
Aniket Bansode <Aniket_Bansode@epam.com>
2021-02-04 20:10:11 +0530

    Css handling

--- src/App.js
+++ src/App.js
@@ -6,63 +6,65 @@ import {ArrowRight, CaretDown, ExclamationCircleFill, FileBreak, Printer } from 
 function App() {
   return (
     <div className="App">
-     <Card className="text-center">
+      <Row>
+      <Col lg={4}></Col> 
+      <Col lg={8}> 
+      <Card className="App-card">
       <Card.Header>
           <Row>
-            <Col lg={8}>
+            <Col lg={7}>
               <div className="App-header">Cancel / recall payment, GBP 1,000.00 </div>
               <div className="App-subheader">1234567890123456 (GB), KUIML Business Company </div>
             </Col>
-            <Col lg={4}>
-              <div className="">
-                <Printer size={20} style={{marginRight:10}}/> | 
-                <FileBreak style={{marginLeft:10}} size={20}/>
-                <Button style={{marginLeft:50}} variant="outline-primary">Reject</Button>
-                <Button style={{marginLeft:10}} variant="primary">Authorize</Button>
+            <Col lg={5}>
+              <div className="App-upper">
+                <FileBreak style={{ marginRight:20}} size={18}/>
+                <span className= "App-border"></span>
+                <Printer size={18} style={{marginLeft:20}} /> 
+                <Button style={{marginLeft:20 }} variant="outline-secondary">Reject</Button>
+                <Button style={{marginLeft:10}} variant="secondary">Authorize</Button>
               </div>
             </Col>
           </Row>
       </Card.Header>
-      <Card.Body>
-        <Row lg={8}>
-          <Col lg={2}>
-          <Card.Text>
-            <div style={{color: 'gray'}}>
+      <Card.Body className="App-cardbody">
+        <Row lg={9} className="App-row">
+          <Col lg={3}>
+          <Card.Text className="App-border">
+            <div className="App-upperText">
              Request Reference
             </div>
-            <div>
+            <div className="App-belowText">
              SET123456BCH 
             </div>
           </Card.Text>
           </Col>
-          <Col lg={1}> <div> | </div>  <div> | </div> </Col>
-          <Col lg={2}>
-            <Card.Text>
-              <div style={{color: 'gray'}}>
+          <Col lg={3} >
+            <Card.Text className="App-border">
+              <div className="App-upperText">
                 Category
               </div>
-              <div>
+              <div className="App-belowText">
                 Payment 
               </div>
             </Card.Text>
           </Col>
-          <Col lg={1}> <div> | </div>  <div> | </div> </Col>
-          <Col lg={2}>
-            <Card.Text>
-              <div style={{color: 'gray'}}>
+          <Col lg={3}>
+            <Card.Text className="App-border">
+              <div className="App-upperText">
                 Request Status
               </div>
-              <div>
-                <ExclamationCircleFill color="yellow"/> Pending 
+              <div className="App-belowText">
+                <ExclamationCircleFill color="yellow" size={15}/> Pending 
               </div>
             </Card.Text>
           </Col>
+          <span className="App-fulldetails" ><a href="#">Full Details <CaretDown/></a></span>
         </Row>
       </Card.Body>
-      <Card.Footer >
-        <div style={{textAlign:'right'}}><a href="#">View Details <CaretDown/></a></div>
-      </Card.Footer>
     </Card>
+      </Col>
+      </Row>
     </div>
   );
 }
